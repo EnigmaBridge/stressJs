cf42be1c2f581a6e30bcc2cbd997d4f1bfdb2fe3
Dusan Klinec <dusan.klinec@gmail.com>
2016-04-14 12:13:02 +0100

    locust: certificate verification disabled, speed up

--- locustfile.py
+++ locustfile.py
@@ -1,24 +1,31 @@
 from locust import HttpLocust, TaskSet, task
 import random
 
+# Disable insecurity warning
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
 class UserBehavior(TaskSet):
     reqs = []
-
     def on_start(self):
         """ load request database """
         self.loadDb()
 
     def loadDb(self):
+        if len(self.reqs) != 0:
+            return
         with open("requests.txt") as f:
             self.reqs = [x.strip() for x in f.readlines()]
         pass
 
     @task(1)
     def index(self):
-        self.client.get(random.choice(self.reqs))
+        # SSL certificate verification is disabled, it is slow, does not matter here. Not testing verification speed...
+        self.client.get(random.choice(self.reqs), verify=False)
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    min_wait=10
-    max_wait=10
+    min_wait=0
+    max_wait=0
 
